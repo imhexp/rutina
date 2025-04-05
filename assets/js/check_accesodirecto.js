957e11a078fc70c2dc65eaaf9ace31758da2f7e5
hexp <hexp@hexp.es>
2025-04-05 17:22:10 +0200

    pruebas

--- assets/js/check_accesodirecto.js
+++ assets/js/check_accesodirecto.js
@@ -1,18 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const novedad = document.getElementById('novedad_accesodirecto');
-  const useragent = navigator.userAgent || window.opera;
-  const ios = /iPad|iPhone|iPod/.test(useragent) && !window.MSStream;
-  const android = /android/i.test(useragent);
-
-  if (ios) {
-    if (window.navigator.standalone) {
+    const novedad = document.getElementById('novedad_accesodirecto');
+    const useragent = navigator.userAgent || window.opera;
+    const ios = /iPad|iPhone|iPod/.test(useragent) && !window.MSStream;
+    const android = /android/i.test(useragent);
+    let check_promptapp;
+  
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      check_promptapp = event;
+      if (novedad) novedad.style.display = 'block';
+    });
+  
+    if (!check_promptapp && novedad) {
+      novedad.style.display = 'none';
+    }
+  
+    if (novedad) {
+      novedad.addEventListener('click', () => {
+        if (check_promptapp) {
+          check_promptapp.prompt();
+          check_promptapp.userChoice.then(() => {
+            check_promptapp = null;
+          });
+        }
+      });
+    }
+  
+    if (ios) {
+      if (window.navigator.standalone) {
+        if (novedad) novedad.style.display = 'none';
+      }
+    } else if (android) {
+      if (window.matchMedia('(display-mode: standalone)').matches) {
+        if (novedad) novedad.style.display = 'none';
+      }
+    } else {
       if (novedad) novedad.style.display = 'none';
     }
-  } else if (android) {
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      if (novedad) novedad.style.display = 'none';
-    }
-  } else {
-    if (novedad) novedad.style.display = 'none';
-  }
-});
+  });
+  
